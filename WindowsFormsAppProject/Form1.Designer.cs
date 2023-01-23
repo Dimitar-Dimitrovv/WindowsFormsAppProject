5f6af779411602f84d948c565dc7138ca5b938db
Dimitar-Dimitrovv <ml05tko@gmail.com>
2023-01-23 02:26:23 +0200

    Add project files.

---
+++ WindowsFormsAppProject/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace WindowsFormsAppProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RectangleButton = new System.Windows.Forms.Button();
+            this.TriangleButton = new System.Windows.Forms.Button();
+            this.CircleButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RectangleButton
+            // 
+            this.RectangleButton.Location = new System.Drawing.Point(12, 12);
+            this.RectangleButton.Name = "RectangleButton";
+            this.RectangleButton.Size = new System.Drawing.Size(75, 23);
+            this.RectangleButton.TabIndex = 0;
+            this.RectangleButton.Text = "Rectangle";
+            this.RectangleButton.UseVisualStyleBackColor = true;
+            this.RectangleButton.Click += new System.EventHandler(this.RectangleButton_Click);
+            // 
+            // TriangleButton
+            // 
+            this.TriangleButton.Location = new System.Drawing.Point(93, 12);
+            this.TriangleButton.Name = "TriangleButton";
+            this.TriangleButton.Size = new System.Drawing.Size(75, 23);
+            this.TriangleButton.TabIndex = 0;
+            this.TriangleButton.Text = "Triangle";
+            this.TriangleButton.UseVisualStyleBackColor = true;
+            this.TriangleButton.Click += new System.EventHandler(this.TriangleButton_Click);
+            // 
+            // CircleButton
+            // 
+            this.CircleButton.Location = new System.Drawing.Point(175, 11);
+            this.CircleButton.Name = "CircleButton";
+            this.CircleButton.Size = new System.Drawing.Size(75, 23);
+            this.CircleButton.TabIndex = 1;
+            this.CircleButton.Text = "Circle";
+            this.CircleButton.UseVisualStyleBackColor = true;
+            this.CircleButton.Click += new System.EventHandler(this.CircleButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CircleButton);
+            this.Controls.Add(this.TriangleButton);
+            this.Controls.Add(this.RectangleButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RectangleButton;
+        private System.Windows.Forms.Button TriangleButton;
+        private System.Windows.Forms.Button CircleButton;
+    }
+}
+
